26f914ccc84c848bcd86cdb73eab42d16aac6790
Lin Zhu <julian.lin.zhu@gmail.com>
2017-10-02 04:14:34 -0400

    10/1

--- 031.next-permutation.java
+++ 031.next-permutation.java
@@ -3,26 +3,18 @@ class Solution {
             if (nums.length < 2) return ;
             int i = nums.length - 2;
             while(i >= 0 && nums[i] >= nums[i + 1]) i--;
-            if(i < 0) {
-                for(i = 0; i < nums.length/2; i++) {
-                    swap(nums, i,nums.length - i -1);
-                }
-                
-            } else {
+            if(i >= 0) {
                 int j = nums.length - 1;
                 while(nums[j] <= nums[i]) j--;
-                swap(nums, j,i);
-           
-                i++;
-                int n = (nums.length -1  + i)/2;
-                int k = nums.length - 1;
-                while(i <= n){
-                    swap(nums,k, i);
-                    i++;
-                    k--;
-                }
+                swap(nums, j,i);  
             }
-            
+            reverse(nums, i);   
+        }
+        private void reverse (int[] nums, int begin){
+            int end = nums.length- 1;
+            while(++begin < end) {
+                swap(nums, begin, end--);
+            }
         }
         private void swap(int[] nums, int a, int b) {
             int tmp = nums[a];
